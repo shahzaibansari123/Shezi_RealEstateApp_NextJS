366b275d4eba9d74207d02264a4e2dc451a46cfe
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-04-20 23:44:14 +0500

    work complete

---
+++ components/Footer.jsx
@@ -0,0 +1,13 @@
+import {Box} from '@chakra-ui/react'
+
+const Footer=()=>(
+    <Box p="5" color="gray.600" borderTop="1px" textAlign="center" borderColor="gray.100">
+        2022 Realtor, Inc.
+
+
+
+    </Box>
+
+)
+
+export default Footer
