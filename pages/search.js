f1e169b9505f2bbff5e2fef14784210b15a6b93d
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-04-21 00:43:25 +0500

    work complete

--- pages/search.js
+++ pages/search.js
@@ -54,3 +54,16 @@ const Search = ({properties}) => {
 };
 
 export default Search;
+
+export async function getStaticProps(){
+    const propertyForSale= await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-sale&hitsPerPage=6`)
+    const propertyForRent= await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-rent&hitsPerPage=6`)
+  
+  return{
+    props:{
+      propertiesForSale: propertyForSale?.hits,
+      propertiesForRent: propertyForRent?.hits,
+    }
+  }
+  
+  }
