2c69b0b0972ddc1447d0797386aa27d8018e0537
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-07-08 17:31:13 +0500

    work complete

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,7 @@ import {Box} from '@chakra-ui/react'
 
 const Footer=()=>(
     <Box p="5" color="gray.600" borderTop="1px" textAlign="center" borderColor="gray.100">
-        2022 Realtor, Inc.
+        2022 Ghar Dhun do, Inc.
 
 
 
